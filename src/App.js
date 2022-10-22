5410d70e6470f89abebc57ff6f8e73e0798761cb
HapaxHypatia <r.haskmann@gmail.com>
2022-10-22 11:43:13 +1000

    Basic UI with search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import data from './data.json'
 
 function App() {
+	const [searchTerm, setSearchTerm] = useState("")
+	const [results, setResults] = useState([])
+	const seenURLs = []
+	const clean_data = data.filter(function(currentObject) {
+    if (currentObject.URL in seenURLs) {
+        return false;
+    } else {
+        seenURLs[currentObject.URL] = true;
+        return true;
+    }
+})
+
+	const updateState = (e) => {
+		const val = e.target.value;
+		setSearchTerm(val);
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		const top_results = clean_data.filter(item=> item.Title.includes(searchTerm))
+		const next_results = clean_data.filter(item=> item.Description.includes(searchTerm))
+		setResults(top_results.concat(next_results))
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	  <>
+		  <form id={'searchForm'} onSubmit={handleSubmit}>
+					<input id='searchBox' value={searchTerm ?? ""} onChange={updateState} placeholder="Search listening resources" />
+					<button>Search</button>
+		  </form>
+		  <div id={"resultsList"}>
+				{results.map((r)=>
+					<div key={String(r.URL)} className={'result'}>
+						<img src={r.Image}/>
+						<a className={'result-link'} target={"_blank"} href={String(r.URL)}>{r.Title}</a>
+						<p className={'level'}>{r.Level}</p>
+						<p>{r.Page_title}</p>
+						<p>{r.Description}</p>
+					</div>
+					)}
+		  </div>
+		  </>);
 }
 
 export default App;
