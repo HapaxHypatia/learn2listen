538d737de44e67965c90a16d3462f6ce3eab6559
HapaxHypatia <r.haskmann@gmail.com>
2022-10-24 21:39:29 +1000

    updating data and styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {useState} from "react";
 import data from './data.json'
 
+
 function App() {
 	const [searchTerm, setSearchTerm] = useState("")
 	const [results, setResults] = useState([])
@@ -14,35 +15,51 @@ function App() {
 			return true;
 		}
 })
+	const seenSites = []
+	const sites = clean_data.filter(function(currentObject) {
+		if (currentObject.Page_title in seenSites) {
+			return false;
+		} else {
+			seenSites[currentObject.Page_title] = true;
+			return true;
+		}
+}
 
+	)
 	const updateState = (e) => {
 		const val = e.target.value;
 		setSearchTerm(val)
-	}
-
-	const handleSubmit = (e) => {
-		e.preventDefault()
 		const top_results = clean_data.filter((item)=> item.Title.toLowerCase().includes(searchTerm.toLowerCase()))
 		const next_results = clean_data.filter(item=> item.Description.toLowerCase().includes(searchTerm))
 		setResults(top_results.concat(next_results))
+
 	}
 
   return (
 	  <>
-		  <form id={'searchForm'} onSubmit={handleSubmit}>
-					<input id='searchBox'value={searchTerm ?? ""} onChange={updateState} placeholder="Search listening resources" />
-					<button>Search</button>
+
+		  <form id={'searchForm'} >
+			  <input id='searchBox'value={searchTerm ?? ""} onChange={updateState} placeholder="Type to search listening resources" />
 		  </form>
 		  <div id={"resultsList"}>
 				{results.map((r)=>
-					<div key={r.URL+r.Current_time} className={'result'}>
+					<div key={Math.random()} className={'result'}>
 						<img src={r.Image}/>
+						<br/>
 						<a className={'result-link'} target={"_blank"} href={String(r.URL)}>{r.Title}</a>
-						<p className={'level'}>{r.Level}</p>
+						{r.Level && <p className={'level'}>{r.Level}</p>}
 						<a href={r.Page_URL}>Source: {r.Page_title}</a>
+						<br/>
 						<p>{r.Description}</p>
 					</div>
 					)}
+		  </div>
+		  <h2>Search {clean_data.length} audio and video resources from {sites.length} sites!</h2>
+		  <div id={'sitesList'}>
+			  {sites.map((item)=>
+				  <span><a href={item.Page_URL}>{item.Page_title}</a></span>
+			  )
+			  }
 		  </div>
 		  </>);
 }
